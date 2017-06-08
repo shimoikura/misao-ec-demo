0c4e441f8d86a5e118af00ac4f05b26d5d7fbc3e
atm0016 <atm0016@example.com>
2017-06-08 14:52:32 +0530

    created cart-add.php

---
+++ cart-add.php
@@ -0,0 +1,19 @@
+<?php include "header.php" ?>
+
+<div class="container" id="cart-add">
+  <div class="row">
+    <div class="col-md-4">
+      <span class="dis_tc glyphicon glyphicon-ok"></span>
+      <img src="images/product-img/1_mm_nature_photography.jpg" alt="">
+      <h2>Gold bracelet</h2>
+    </div>
+
+    <div class="col-md-4 cart-subtotal-box">
+      <p id="cart-subtotal">Cart subtotal : <span>Rs 400</span></p>
+    </div>
+    <div class="col-md-4 cart-subtotal-box">
+      <input type="submit" class="btn green" name="" value="CART">
+      <input type="submit" class="btn blue" name="" value="CHECKOUT">
+    </div>
+  </div>
+</div>
