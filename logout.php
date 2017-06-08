2d11afef9f8b988af2d092ff426671af88726a22
atm0016 <atm0016@example.com>
2017-06-08 09:06:02 +0530

    when logout, deleate sesstion

--- logout.php
+++ logout.php
@@ -2,10 +2,7 @@
 session_start();
 $data = $_POST['data'];
 if ($data == 1) {
-  // unset($_SESSION["login_user"]);
   $_SESSION = array();
-  // header("location: index.php" );
-  // exit();
   echo "logout!";
 }
 else {
